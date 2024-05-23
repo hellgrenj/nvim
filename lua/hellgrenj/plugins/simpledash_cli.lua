71c8210c6d826c619f9f29d1fc1fa15a85f5b379
hellgrenj <hellgrenj@gmail.com>
2024-05-23 21:59:21 +0200

    minor cleanup

--- lua/hellgrenj/plugins/simpledash_cli.lua
+++ lua/hellgrenj/plugins/simpledash_cli.lua
@@ -2,7 +2,7 @@ local simpledash_dir = vim.fn.expand("~/simpledash_cli.nvim")
 if vim.fn.isdirectory(simpledash_dir) ~= 0 then
     -- Return the plugin configuration if the directory exists
     return {
-        dir = simpledash_dir, 
+        dir = simpledash_dir,
         config = function()
             require("simpledash_cli").setup()
         end
