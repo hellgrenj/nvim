bd7a47a1e2c45ebda98e22e91f2fc4c4eae460d7
hellgrenj <hellgrenj@gmail.com>
2024-09-25 14:17:37 +0200

    now using llama3.1 in gen.nvim

--- lua/hellgrenj/plugins/gen.lua
+++ lua/hellgrenj/plugins/gen.lua
@@ -4,7 +4,7 @@ return {
    config = function()
         require("gen").setup({
             -- The default model to use.
-            model = "llama3",
+            model = "llama3.1",
             -- The host running the Ollama service.
             host = "localhost",
             -- The port on which the Ollama service is listening.
