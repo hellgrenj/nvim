8102727b499728950e75e30e20b933d530dbb7c2
hellgrenj <hellgrenj@gmail.com>
2024-11-02 14:13:33 +0100

    we are now exiting zenmode and keeping the current buffer

--- lua/hellgrenj/plugins/zenmode.lua
+++ lua/hellgrenj/plugins/zenmode.lua
@@ -1,73 +1,51 @@
 return {
     'folke/zen-mode.nvim',
     config = function()
+        -- Define the toggle function within config
+        local function toggleZenMode()
+            local zen_mode_active = require("zen-mode.view").is_open()
+
+            if zen_mode_active then
+                local current_buf = vim.api.nvim_get_current_buf()
+                require("zen-mode").toggle()
+                vim.defer_fn(function()
+                    vim.api.nvim_set_current_buf(current_buf)
+                end, 10)
+            else
+                require("zen-mode").toggle()
+            end
+        end
+
+        -- Zen Mode configuration
         require("zen-mode").setup({
             window = {
-                backdrop = 1, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-                -- height and width can be:
-                -- * an absolute number of cells when > 1
-                -- * a percentage of the width / height of the editor when <= 1
-                -- * a function that returns the width or the height
-                width = 120, -- width of the Zen window
-                height = 0.90, -- height of the Zen window
-                -- by default, no options are changed for the Zen window
-                -- uncomment any of the options below, or add other vim.wo options you want to apply
-                options = {
-                    -- signcolumn = "no", -- disable signcolumn
-                    -- number = false, -- disable number column
-                    -- relativenumber = false, -- disable relative numbers
-                    -- cursorline = false, -- disable cursorline
-                    -- cursorcolumn = false, -- disable cursor column
-                    -- foldcolumn = "0", -- disable fold column
-                    -- list = false, -- disable whitespace characters
-                },
+                backdrop = 1,
+                width = 120,
+                height = 0.90,
+                options = {},
             },
             plugins = {
-                -- disable some global vim options (vim.o...)
-                -- comment the lines to not apply the options
                 options = {
                     enabled = true,
-                    ruler = false, -- disables the ruler text in the cmd line area
-                    showcmd = false, -- disables the command in the last line of the screen
-                    -- you may turn on/off statusline in zen mode by setting 'laststatus'
-                    -- statusline will be shown only if 'laststatus' == 3
-                    laststatus = 0, -- turn off the statusline in zen mode
+                    ruler = false,
+                    showcmd = false,
+                    laststatus = 0,
                 },
-                twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
-                gitsigns = { enabled = true }, -- disables git signs
-                tmux = { enabled = true }, -- disables the tmux statusline
-                -- this will change the font size on kitty when in zen mode
-                -- to make this work, you need to set the following kitty options:
-                -- - allow_remote_control socket-only
-                -- - listen_on unix:/tmp/kitty
-                kitty = {
-                    enabled = false,
-                    font = "+4", -- font size increment
-                },
-                -- this will change the font size on alacritty when in zen mode
-                -- requires  Alacritty Version 0.10.0 or higher
-                -- uses `alacritty msg` subcommand to change font size
-                alacritty = {
-                    enabled = false,
-                    font = "14", -- font size
-                },
-                -- this will change the font size on wezterm when in zen mode
-                -- See alse also the Plugins/Wezterm section in this projects README
-                wezterm = {
-                    enabled = false,
-                    -- can be either an absolute font size or the number of incremental steps
-                    font = "+4", -- (10% increase per step)
-                },
+                twilight = { enabled = false },
+                gitsigns = { enabled = true },
+                tmux = { enabled = true },
+                kitty = { enabled = false, font = "+4" },
+                alacritty = { enabled = false, font = "14" },
+                wezterm = { enabled = false, font = "+4" },
             },
-            -- callback where you can add custom code when the Zen window opens
-            on_open = function(win)
-            end,
-            -- callback where you can add custom code when the Zen window closes
-            on_close = function()
-            end,
         })
-        -- set keybindin toggle ZenMode with leader + z + n
-        vim.api.nvim_set_keymap("n", "<leader>zn", ":ZenMode<CR>", { noremap = true, silent = true })
 
+        -- Map the local toggle function directly
+        vim.api.nvim_set_keymap("n", "<leader>zn", "", {
+            noremap = true,
+            silent = true,
+            callback = toggleZenMode
+        })
     end
 }
+
