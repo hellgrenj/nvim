28d3303c730f8796035cae11a3ce52b816a95811
hellgrenj <hellgrenj@gmail.com>
2024-11-03 17:30:49 +0100

    toggle diffview on leader+dv instead of an open and a close keybinding

--- lua/hellgrenj/plugins/diffview.lua
+++ lua/hellgrenj/plugins/diffview.lua
@@ -1,8 +1,17 @@
 return {
     "sindrets/diffview.nvim",
     config = function()
-        vim.keymap.set('n', '<leader>dvo', require('diffview').open) 
-        vim.keymap.set('n', '<leader>dvc', require('diffview').close)
+        local diffview = require('diffview')
+        local is_diffview_open = false -- diffview does not expose an is_open()
+        vim.keymap.set('n', '<leader>dv', function()
+            if is_diffview_open then
+                diffview.close()
+                is_diffview_open = false
+            else
+                diffview.open()
+                is_diffview_open = true
+            end
+        end)
     end
 }
-    
+
