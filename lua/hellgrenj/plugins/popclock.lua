8e083282895a7194bbe6a3187839a26a73ac491c
hellgrenj <hellgrenj@gmail.com>
2024-11-07 12:53:48 +0100

    going back to my minimalistic popclock for now..

--- lua/hellgrenj/plugins/popclock.lua
+++ lua/hellgrenj/plugins/popclock.lua
@@ -4,7 +4,8 @@ if vim.fn.isdirectory(popclock_dir) ~= 0 then
     return {
         dir = popclock_dir,
         config = function()
-            require("popclock").setup({ key_binding = "<leader>kl", position = "center", bgcolor="#FFFFFF", fgcolor="#000000" })
+            -- require("popclock").setup({ key_binding = "<leader>kl", position = "center", bgcolor="#FFFFFF", fgcolor="#000000" })
+            require("popclock").setup({key_binding = "<leader>kl"})
         end
     }
 else
