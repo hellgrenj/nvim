ee769b6a02d957afe9da2e828b39979e24fdd8fa
hellgrenj <hellgrenj@gmail.com>
2024-12-15 11:54:50 +0100

    removed bqf and added custom keybindings for quickfix

--- lua/hellgrenj/remap.lua
+++ lua/hellgrenj/remap.lua
@@ -6,3 +6,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- move down/up half a page and center in the middle
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true })
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
+
+-- quickfix up and down
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
