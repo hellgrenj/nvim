ec005b4d0be1082ea4f79353f8acbffe387d22f8
hellgrenj <hellgrenj@gmail.com>
2024-12-31 13:41:14 +0100

    added notification when toggling copilot for zen mode and made fidget override vim.notify

--- lua/hellgrenj/plugins/copilot.lua
+++ lua/hellgrenj/plugins/copilot.lua
@@ -1,8 +1,10 @@
 return {
     'github/copilot.vim',
     config = function()
-        -- toggle github copilot
-        vim.api.nvim_set_keymap('n', '<leader>cp', '<Cmd>lua vim.g.copilot_enabled = not vim.g.copilot_enabled<CR>',
+        -- Toggle GitHub Copilot with notification (fidget set to override vim.notify)
+        vim.api.nvim_set_keymap('n', '<leader>cp',
+            '<Cmd>lua vim.g.copilot_enabled = not vim.g.copilot_enabled; vim.notify("Copilot " .. (vim.g.copilot_enabled and "ON" or "OFF"), vim.log.levels.INFO)<CR>',
             { noremap = true, silent = true })
     end
 }
+
--- lua/hellgrenj/plugins/fidget.lua
+++ lua/hellgrenj/plugins/fidget.lua
@@ -1,6 +1,11 @@
 return {
     "j-hui/fidget.nvim",
     config = function()
-        require('fidget').setup()
+        require('fidget').setup({
+            -- Options related to notification subsystem
+            notification = {
+                override_vim_notify = true
+            },
+        })
     end
 }
