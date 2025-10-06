d3b113b39c53e835324f109ab32def3a3f6f8e03
Johan Hellgren <johan.hellgren@tietoevry.com>
2025-10-06 14:56:46 +0200

    added nerdfonts to dadbod, a little nicer

--- lua/hellgrenj/plugins/dadbod-stuff.lua
+++ lua/hellgrenj/plugins/dadbod-stuff.lua
@@ -1,5 +1,14 @@
 return {
+  {
     'tpope/vim-dadbod',
-    'kristijanhusak/vim-dadbod-ui',
-    'kristijanhusak/vim-dadbod-completion',
+    dependencies = {
+      'kristijanhusak/vim-dadbod-ui',
+      'kristijanhusak/vim-dadbod-completion',
+    },
+    init = function()
+      -- Enable Nerd Font icons in DBUI
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
 }
+
