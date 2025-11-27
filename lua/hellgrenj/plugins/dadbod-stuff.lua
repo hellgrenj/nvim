2f0f9b4fc2a70e884d2c28a4cbd07f72a9b26601
Johan Hellgren <johan.hellgren@tietoevry.com>
2025-11-27 08:56:40 +0100

    dadbod stuff

--- lua/hellgrenj/plugins/dadbod-stuff.lua
+++ lua/hellgrenj/plugins/dadbod-stuff.lua
@@ -1,3 +1,18 @@
+-- return {
+--   {
+--     'tpope/vim-dadbod',
+--     dependencies = {
+--       'kristijanhusak/vim-dadbod-ui',
+--       'kristijanhusak/vim-dadbod-completion',
+--     },
+--     init = function()
+--       -- Enable Nerd Font icons in DBUI
+--       vim.g.db_ui_use_nerd_fonts = 1
+--     end,
+--   },
+-- }
+--
+--
 return {
   {
     'tpope/vim-dadbod',
@@ -8,6 +23,17 @@ return {
     init = function()
       -- Enable Nerd Font icons in DBUI
       vim.g.db_ui_use_nerd_fonts = 1
+    end,
+    config = function()
+      -- Make dadbod output (dbout) readable: real columns, no folding/wrapping
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "dbout",
+        callback = function()
+          vim.opt_local.wrap = false        -- don't wrap lines
+          vim.opt_local.foldenable = false  -- disable folds
+          vim.opt_local.list = false        -- hide · and other listchars
+        end,
+      })
     end,
   },
 }
