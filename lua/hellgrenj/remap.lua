25c469c2053330a9c33c8c837207bc83f20f470c
Johan Hellgren <johan.hellgren@tietoevry.com>
2026-02-15 09:33:53 +0100

    added a simple keybinding for copying current filepath to clipboard

--- lua/hellgrenj/remap.lua
+++ lua/hellgrenj/remap.lua
@@ -11,3 +11,10 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
 vim.keymap.set("n", "<leader>cj", "<cmd>cnext<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>ck", "<cmd>cprev<CR>", { noremap = true })
 
+-- copy current filepath to clipboard and echo out..
+vim.keymap.set("n", "<leader>fp", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  print(path .. " copied to clipboard!")
+end, { desc = "Copy full file path and echo" })
+
