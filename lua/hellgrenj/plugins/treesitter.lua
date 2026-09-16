aed09338b874b07ecc44f2dd8ab7da29c191d037
hellgrenj <hellgrenj@gmail.com>
2026-09-16 19:30:06 +0200

    s

--- lua/hellgrenj/plugins/treesitter.lua
+++ lua/hellgrenj/plugins/treesitter.lua
@@ -1,35 +1,32 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all"
-            ensure_installed = {
-                "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash", "go",
-            },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            indent = {
-                enable = true
-            },
-
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = { "markdown" },
-            },
+        require("nvim-treesitter").install({
+            "vimdoc",
+            "javascript",
+            "typescript",
+            "c",
+            "lua",
+            "rust",
+            "jsdoc",
+            "bash",
+            "go",
+            "markdown",
+            "markdown_inline",
         })
-    end
+
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function(args)
+                if pcall(vim.treesitter.start, args.buf) then
+                    vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                    if vim.bo[args.buf].filetype == "markdown" then
+                        vim.bo[args.buf].syntax = "on"
+                    end
+                end
+            end,
+        })
+    end,
 }
